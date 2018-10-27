885bff28428c7d6fb234fb44ccc2241ab9e4fe5f
Michael Wu <38211512+mwu412@users.noreply.github.com>
2018-10-27 16:10:15 +0800

    Create packages_pip_install.sh

---
+++ packages_pip_install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+## macbook has python2 preinstalled
+
+## --- Linux --- ##
+## install pip
+sudo apt install python3-pip
+
+sudo apt-get install python3-tk
+
+## --- Mac --- ##
+## update pip
+# python3 -mpip install -U pip
+
+##
+pip3 install numpy
+
+##
+pip3 install scipy
+
+##
+pip3 install pandas
+
+## 
+pip3 install matplotlib
+
+##
+pip3 install seaborn
+
+##
+pip3 install jupyter
+
+##
+pip3 install ipython
+
+##
+pip3 install sympy
+
+## pylint: a source code, bug and quality checker for the Python programming language
+pip3 install pylint
